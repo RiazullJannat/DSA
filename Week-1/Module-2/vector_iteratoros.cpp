7b4982b46a3d5c0a4bd36a0b031615b6538ac90b
RJannat <ramimk097@gmail.com>
2025-06-21 20:36:02 +0600

    practic iterators in vector

---
+++ Week-1/Module-2/vector_iteratoros.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    vector v = {1, 2, 3};
+    for (auto it = v.begin(); it < v.end(); it++)
+    {
+        cout << *it << " ";
+    }
+    return 0;
+}

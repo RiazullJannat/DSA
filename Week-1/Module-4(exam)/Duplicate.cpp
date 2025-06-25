05f11be28d8bc00aea1f29b7a6853320add99daf
RJannat <ramimk097@gmail.com>
2025-06-25 22:53:21 +0600

    solved a contest problem taht is easy

---
+++ Week-1/Module-4(exam)/Duplicate.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    string flag = "NO";
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = i + 1; j < n; j++)
+        {
+            if (a[i] == a[j])
+            {
+                flag = "YES";
+            }
+        }
+    }
+    cout << flag << endl;
+    return 0;
+}

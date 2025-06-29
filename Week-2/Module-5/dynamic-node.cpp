dd37fce6daa07902c1f3bb6ad953cb14626cafb8
RJannat <ramimk097@gmail.com>
2025-06-29 09:05:48 +0600

    dynamic node

--- Week-2/Module-5/dynamic-node.cpp
+++ Week-2/Module-5/dynamic-node.cpp
@@ -16,5 +16,7 @@ int main()
     Node *head = new Node(10);
     Node *b = new Node(20);
     head->next = b;
+    cout << head->value << endl;
+    cout << head->next->value << endl;
     return 0;
 }

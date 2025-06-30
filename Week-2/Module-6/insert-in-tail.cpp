608af719851f23f898f82503eb3d1d0e11fba469
RJannat <ramimk097@gmail.com>
2025-06-30 10:11:51 +0600

    inset operation at tail

---
+++ Week-2/Module-6/insert-in-tail.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Node
+{
+public:
+    int value;
+    Node *next;
+    Node(int val)
+    {
+        this->value = val;
+        this->next = NULL;
+    }
+};
+void insert_to_tail(Node *&head, int val)
+{
+    Node *newNode = new Node(val);
+    Node *temp = head;
+    while (temp->next != NULL)
+    {
+        temp = temp->next;
+    }
+    temp->next = newNode;
+}
+void print_linked_list(Node *head)
+{
+    Node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->value << endl;
+        temp = temp->next;
+    }
+}
+int main()
+{
+    Node *head = new Node(10);
+    Node *a = new Node(20);
+    Node *b = new Node(30);
+    head->next = a;
+    a->next = b;
+    insert_to_tail(head, 100);
+    print_linked_list(head);
+    return 0;
+}

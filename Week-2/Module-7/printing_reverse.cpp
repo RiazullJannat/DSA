b847f6f4f006392f9d362bdee714970a700def96
RJannat <ramimk097@gmail.com>
2025-07-02 07:59:20 +0600

    printing linked list in reverse order

---
+++ Week-2/Module-7/printing_reverse.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Node
+{
+public:
+    int value;
+    Node *next;
+    Node(int val)
+    {
+        this->value = val;
+        this->next = NULL;
+    }
+};
+void insert_at_tail(Node *&head, Node *&tail, int value)
+{
+    Node *newNode = new Node(value);
+    if (head == NULL)
+    {
+        head = newNode;
+        tail = newNode;
+    }
+    tail->next = newNode;
+    tail = newNode;
+}
+void print_reverse(Node *head)
+{
+
+    if (head == NULL)
+    {
+        return;
+    }
+    print_reverse(head->next);
+    cout << head->value << endl;
+}
+
+int main()
+{
+    Node *head = NULL;
+    Node *tail = NULL;
+    int val;
+    while (true)
+    {
+        cin >> val;
+        if (val == -1)
+        {
+            break;
+        }
+        insert_at_tail(head, tail, val);
+    }
+    print_reverse(head);
+    return 0;
+}

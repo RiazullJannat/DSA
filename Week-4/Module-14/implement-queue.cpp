1cb2f0e2390b246acb932eb0e1db6f7a8d1e0423
RJannat <ramimk097@gmail.com>
2025-07-21 11:59:10 +0600

    implement queue data structure

---
+++ Week-4/Module-14/implement-queue.cpp
@@ -0,0 +1,75 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Node
+{
+public:
+    int value;
+    Node *next;
+    Node(int val)
+    {
+        this->value = val;
+        this->next = NULL;
+    }
+};
+class Myqueue
+{
+public:
+    Node *head = NULL;
+    Node *tail = NULL;
+    int sz = 0;
+    void push(int val)
+    {
+        Node *newNode = new Node(val);
+        sz++;
+        if (head == NULL)
+        {
+            head = newNode;
+            tail = newNode;
+            return;
+        }
+        tail->next = newNode;
+        tail = newNode;
+    }
+    void pop()
+    {
+        if (head == NULL)
+        {
+            return;
+        }
+        sz--;
+        if (head == tail)
+        {
+            head = NULL;
+            tail = NULL;
+            return;
+        }
+        Node *delNode = head;
+        head = head->next;
+        delete delNode;
+    }
+    int front()
+    {
+        if (head != NULL)
+            return head->value;
+        return -1;
+    }
+    int back()
+    {
+        if (tail != NULL)
+            return tail->value;
+        return -1;
+    }
+    int size()
+    {
+        return sz;
+    }
+    bool empty()
+    {
+        return head == NULL;
+    }
+};
+int main()
+{
+
+    return 0;
+}

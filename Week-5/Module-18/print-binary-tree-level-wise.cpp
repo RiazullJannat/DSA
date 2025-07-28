d5cc951e859e68192ecf579865439534b4706c76
RJannat <ramimk097@gmail.com>
2025-07-28 13:07:05 +0600

    print and input a binary tree

---
+++ Week-5/Module-18/print-binary-tree-level-wise.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Node
+{
+public:
+    int value;
+    Node *left;
+    Node *right;
+    Node(int val)
+    {
+        this->value = val;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+void printTree(Node *root)
+{
+    queue<Node *> tq;
+    tq.push(root);
+    while (!tq.empty())
+    {
+        Node *temp = tq.front();
+        if (temp->left)
+            tq.push(temp->left);
+        if (temp->right)
+            tq.push(temp->right);
+        cout << temp->value << " ";
+        tq.pop();
+    }
+}
+int main()
+{
+    Node *root = new Node(10);
+    Node *a = new Node(20);
+    Node *b = new Node(30);
+    Node *c = new Node(40);
+    Node *d = new Node(50);
+    Node *e = new Node(60);
+    root->left = a;
+    a->left = c;
+    root->right = b;
+    b->left = d;
+    b->right = e;
+    printTree(root);
+    return 0;
+}
